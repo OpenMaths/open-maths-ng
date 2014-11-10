c8c9cc88866cc696274cfd5a3cfde4ccdf1cf81f
Slavomir Vojacek <slavomir@adbrain.com>
2014-11-10 14:49:40 +0000

    disabled spinner loading bar

--- www/lib/app.js
+++ www/lib/app.js
@@ -1,34 +1,36 @@
 var app = angular.module("openMathsApp", [
-		"ngRoute",
-		"angular-loading-bar"
-	]).config(function($routeProvider, $locationProvider) {
+	"ngRoute",
+	"angular-loading-bar"
+]).config(function ($routeProvider, $locationProvider) {
 
-		$routeProvider
-			.when("/", {
-				templateUrl: "views/home.html",
-				controller: "HomeController"
-			})
-			.when("/editor", {
-				templateUrl: "views/editor.html",
-				controller: "EditorController"
-			})
-			.when("/ooops", {
-				templateUrl: "views/ooops.html",
-				controller: "OoopsController"
-			})
-			.when("/sass", {
-				templateUrl: "views/sass.html",
-				controller: "SassController"
-			})
-			.otherwise({
-				redirectTo: "/ooops"
-			});
+	$routeProvider
+		.when("/", {
+			templateUrl: "views/home.html",
+			controller: "HomeController"
+		})
+		.when("/editor", {
+			templateUrl: "views/editor.html",
+			controller: "EditorController"
+		})
+		.when("/ooops", {
+			templateUrl: "views/ooops.html",
+			controller: "OoopsController"
+		})
+		.when("/sass", {
+			templateUrl: "views/sass.html",
+			controller: "SassController"
+		})
+		.otherwise({
+			redirectTo: "/ooops"
+		});
 
-			$locationProvider.html5Mode(true);
-			$locationProvider.hashPrefix("!");
-	});
+	$locationProvider.html5Mode(true);
+	$locationProvider.hashPrefix("!");
+}).config(["cfpLoadingBarProvider", function (cfpLoadingBarProvider) {
+	cfpLoadingBarProvider.includeSpinner = false;
+}]);
 
-app.run(function($rootScope) {
+app.run(function ($rootScope) {
 	$rootScope.siteName = appConfig.siteName;
 	$rootScope.siteLanguage = appConfig.siteLanguage;
 	$rootScope.description = appConfig.description;
