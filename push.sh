b796707c15edaddc0dfcf1d006060f9112cb4b88
Slavomir Vojacek <slavomir@adbrain.com>
2014-11-25 17:07:20 +0000

    Edited push script

--- push.sh
+++ push.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
-git add -A
-git commit -m "${1}"
-git push origin $2
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+git status
+
+printf "\nDo you wish to commit and push to branch ${branch_name}?\nThe commit message is: ${1}\n"
+
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+		git add -A
+		git commit -m "${1}"
+		git push origin branch_name
+		
+		break;;
+        No )
+		echo "Ok, maybe next time :-)"
+		exit;;
+    esac
+done
