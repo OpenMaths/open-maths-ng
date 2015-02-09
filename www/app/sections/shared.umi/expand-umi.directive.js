cb89f65b4584095f22fae3602ac64f537e781514
Slavomir Vojacek <slavomir@adbrain.com>
2015-02-09 14:53:28 +0000

    init expand-umi directive

---
+++ www/app/sections/shared.umi/expand-umi.directive.js
@@ -0,0 +1,36 @@
+(function () {
+	"use strict";
+
+	angular
+		.module("omApp")
+		.directive("expandUmi", expandUmiDirective)
+		.constant("magicForExpandUmiDirective", {
+			directions: ["up", "right", "down", "left"]
+		});
+
+	function expandUmiDirective(magicForExpandUmiDirective) {
+		var directive = {
+			restrict: "E",
+			templateUrl: "app/sections/shared.umi/expand-umi.layout.html",
+			scope: {
+				id: "="
+			},
+			link: link
+		};
+
+		return directive;
+
+		function link(scope) {
+			scope.directions = magicForExpandUmiDirective.directions;
+
+			console.log("expandUmi directive linker init");
+			console.log(scope.id);
+
+			scope.position = function() {
+				console.log("waa");
+			};
+		}
+
+	}
+
+})();
---
+++ www/app/sections/shared.umi/test.js
@@ -0,0 +1,2 @@
+// do not send the same query twice when user backspaces some of the characters and the input is the same as it was on the last call
+// implement retries?
