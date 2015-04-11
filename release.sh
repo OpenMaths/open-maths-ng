d205be9b09f3e153f57e6e3753df306826a0caa0
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-04-12 00:52:04 +0100

    release script

--- release.sh
+++ release.sh
@@ -1,1 +1,6 @@
-cp www/* dist/
+cp -a www/. dist/
+
+mv -v dist/app/om.js dist && rm -r dist/app && mkdir dist/app && mv -v dist/om.js dist/app/om.js
+mv -v dist/assets/css/screen.min.css dist/assets && rm -r dist/assets/css && mkdir dist/assets/css && mv -v dist/assets/screen.min.css dist/assets/css/screen.min.css
+
+divshot push staging
