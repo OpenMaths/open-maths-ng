65c0ed4e4eff0f098a57dca122e48dd3179116c7
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-04-12 09:47:00 +0100

    edit release script

--- release.sh
+++ release.sh
@@ -1,7 +1,9 @@
+# Copy the www folder over to dist
 cp -a www/. dist/
 
-mv -v dist/app/om.js dist && rm -r dist/app && mkdir dist/app && mv -v dist/om.js dist/app/om.js
+# Get rid of redundant files in a hack
+mv -v dist/app/om.js dist && mv -v dist/app/sections dist && rm -r dist/app && mkdir dist/app && mv -v dist/om.js dist/app/om.js && mv -v dist/sections dist/app/sections
 mv -v dist/assets/css/screen.min.css dist/assets && rm -r dist/assets/css && mkdir dist/assets/css && mv -v dist/assets/screen.min.css dist/assets/css/screen.min.css
 
-divshot login
+# Make sure we are logged in
 divshot push staging
