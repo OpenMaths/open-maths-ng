e81bfaaf1ceeab22910b03cee21e7a4bc17d47fc
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-05-07 14:53:22 +0100

    API infrastructure tests

--- app/components/home/Home.Controller.ts
+++ app/components/home/Home.Controller.ts
@@ -1,26 +1,51 @@
 /// <reference path="../../_ref.ts" />
 
 module openmaths {
-	'use strict';
+    'use strict';
 
-	export interface HomeControllerScope extends ng.IScope {
-		name: string;
-		changeName(name: string): void;
-	}
+    export interface HomeControllerScope extends ng.IScope {
+        name: string;
+        changeName(name: string): void;
+    }
 
-	export class HomeController {
-		constructor($scope: HomeControllerScope, Api: openmaths.Api) {
-			$scope.name = 'Test';
+    export class HomeController {
+        constructor($scope: HomeControllerScope, Api: openmaths.Api) {
+            $scope.name = 'Test';
 
-			$scope.changeName = (name) => {
-				$scope.name = name;
-			};
+            $scope.changeName = (name) => {
+                $scope.name = name;
+            };
 
-			console.log(Api.get("https://api.github.com/users/slavomirvojacek/repos"));
-		}
-	}
+            Rx.Observable.fromPromise(Api.get('search/a'))
+                .map(function(d) {
+                    var omResponse = openmaths.Api.response(d);
 
-	angular
-		.module('openmaths')
-		.controller('HomeController', HomeController);
+                    console.debug('omResponse:');
+                    console.info(omResponse);
+
+                    return Rx.Observable.fromPromise(Api.get('https://api.github.com/users/slavomirvojacek/repos', true))
+                        .map(function(d) {
+                            var ghResponse = openmaths.Api.response(d, true);
+
+                            console.debug('ghResponse:');
+                            console.info(ghResponse);
+
+                            return ghResponse.data;
+                        })
+                        .where(function(data) {
+                            return data.length > 0;
+                        });
+                })
+                .switch()
+                .retry(3)
+                .subscribe(function (d) {
+                    console.debug('Response:');
+                    console.log(d);
+                });
+        }
+    }
+
+    angular
+        .module('openmaths')
+        .controller('HomeController', HomeController);
 }
--- app/shared/Api/Api.ts
+++ app/shared/Api/Api.ts
@@ -1,21 +1,60 @@
 /// <reference path="../../_ref.ts" />
 
+// @TODO
+// This service can only be used in combination with RxJS. I think we need an Rx intermediary,
+// which will be responsible for creating observables from angular event streams (model change),
+// as well as some which will be able to catch error codes and send corresponding response without
+// terminating the stream and jumping right into the error handler (causes the app to time out).
+
 module openmaths {
-	'use strict';
+    'use strict';
 
-	export class Api {
-		private http: any;
+    export interface ApiResponse {
+        headers: Object;
+        method: string;
+        url: string;
+        statusCode: number;
+        status: string;
+        data: any;
+    }
 
-		constructor($http: ng.IHttpService) {
-			this.http = $http;
-		}
+    export class Api {
+        private http: any;
+        private api: string;
 
-		get(url: string) {
-			return this.http.get(url);
-		}
-	}
+        constructor($http: ng.IHttpService) {
+            this.http = $http;
+            this.api = 'http://api.om.dev/';
+        }
 
-	angular
-		.module('openmaths')
-		.service('Api', Api);
+        get = (url: string, ignoreApi?: boolean): ng.IHttpPromise<void> => {
+            return this.http.get((ignoreApi ? '' : this.api) + url);
+        };
+
+        post = (url: string, data: Object | string, ignoreApi?: boolean): ng.IHttpPromise<void> => {
+            return this.http.post((ignoreApi ? '' : this.api) + url, data);
+        };
+
+        put = (url: string, data: Object | string, ignoreApi?: boolean): ng.IHttpPromise<void> => {
+            return this.http.put((ignoreApi ? '' : this.api) + url, data);
+        };
+
+        // @TODO
+        // We can possibly add all the verbs, but I guess there is no point in doing so unless we actually use them.
+
+        static response = (d: ng.IHttpPromiseCallbackArg<any>, ignoreApi?: boolean): ApiResponse => {
+            return {
+                headers: d.config.headers,
+                method: d.config.method,
+                url: d.config.url,
+                statusCode: d.status,
+                status: ignoreApi ? d.statusText : _.first(_.keys(d.data)),
+                data: ignoreApi ? d.data : _.first(_.values(d.data))
+            };
+        }
+    }
+
+    angular
+        .module('openmaths')
+        .service('Api', Api);
 }
