d7b82c2c942a5dd80c4032e647b4484b448c3187
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-07-05 18:39:57 +0100

    board/dive toggling kinda works

--- app/components/Explore/Explore.ts
+++ app/components/Explore/Explore.ts
@@ -1,16 +1,50 @@
 module openmaths {
     'use strict';
 
+    interface IDive {
+        state: string;
+    }
+
+    class Dive implements IDive {
+        state = 'explore';
+    }
+
+    interface IBoard {
+        state: string;
+    }
+
+    class Board implements IBoard {
+        state = 'explore.board';
+    }
+
     export class ExploreController {
+        Board = new Board();
+        Dive = new Dive();
+
         triggerBoard: (uriFriendlyTitle: string) => void;
         view: string;
 
-        constructor($state: ng.ui.IStateService) {
-            this.triggerBoard = (uriFriendlyTitle: string) => {
-                console.log(uriFriendlyTitle);
+        constructor($rootScope: ng.IScope, $state: ng.ui.IStateService) {
+            this.updateState($state.current.name);
 
+            $rootScope.$on('$stateChangeSuccess', (e, toState) => {
+                this.updateState(toState.name);
+                console.log(this.view);
+            });
+
+            this.triggerBoard = (uriFriendlyTitle: string) => {
                 $state.go('explore.board', {uriFriendlyTitle: uriFriendlyTitle});
-                this.view = 'board';
+            }
+        }
+
+        updateState(toState: string) {
+            switch (toState) {
+                case this.Board.state:
+                    this.view = 'board';
+                    break;
+                case this.Dive.state:
+                    this.view = 'dive';
+                    break;
             }
         }
     }
