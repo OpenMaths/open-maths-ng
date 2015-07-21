74dc1613b0b98d2c5eb28581f4ee7afa1a836082
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-07-22 00:32:24 +0100

    expandUmi fixes

--- app/_shared/Umi/ExpandUmiDirective.ts
+++ app/_shared/Umi/ExpandUmiDirective.ts
@@ -3,29 +3,33 @@ module openmaths {
 
     interface IExpandUmiDirectiveScope extends ng.IScope {
         directions: string[];
-        label: string;
-        umi: openmaths.Umi;
+        expandId: string;
+        expandLabel: string;
+        //umi: openmaths.Umi;
     }
 
     interface IExpandUmiAttr extends ng.IAttributes {
-        label: string;
+        expandId: string;
+        expandLabel: string;
+        //umi: openmaths.Umi;
     }
 
     export class ExpandUmiDirective implements ng.IDirective {
         link;
         restrict = 'A';
         scope = {
-            umi: '='
+            //umi: '='
         };
         templateUrl = 'app/_shared/Umi/expandUmi.html';
 
         constructor() {
             this.link = (scope: IExpandUmiDirectiveScope, ele, attr: IExpandUmiAttr) => {
                 scope.$watch('umi.htmlContent', () => {
-                    if (scope.umi.empty) return false;
+                    //if (scope.umi.empty) return false;
 
+                    scope.expandId = attr.expandId;
+                    scope.expandLabel = attr.expandLabel;
                     scope.directions = ['top', 'right', 'bottom', 'left'];
-                    scope.label = attr.label;
                 });
             };
         }
