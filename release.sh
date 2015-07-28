a9d954f75bd9715b0fe5ec110c0906053d1449ed
Slavomir Vojacek <slavomirvojacek@gmail.com>
2015-07-28 10:14:07 +0100

    ops, added temp release script

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+printf "\n\n\nDo you wish to release to staging?\n\n"
+
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes )
+			gulp concatVendor typescript && gulp test
+
+#			divshot login
+
+			rm -r dist
+			mkdir dist && mkdir dist/app && mkdir dist/app/dist
+
+			cp app/dist/app.js dist/app/dist && cp app/dist/vendor.js dist/app/dist
+			cp index.html dist/index.html
+
+#			divshot push staging
+
+#			printf "\n\n\nSuccessfully released to staging\n\n"
+            printf "\n\n\nThis step has been temporarily skipped\n\n"
+
+			break;;
+		No )
+			printf "\n\n\nOk, maybe next time :-)\n\n"
+			break;;
+	esac
+done
+
+printf "\n\n\nDo you wish to update production with latest staging release?\n\n"
+
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes )
+#			divshot promote staging production
+
+#			printf "\n\n\nSuccessfully released to production\n\n"
+			printf "\n\n\nThis step has been temporarily skipped\n\n"
+
+			break;;
+		No )
+			printf "\n\n\nOk, maybe next time :-)\n\n"
+			exit;;
+	esac
+done
